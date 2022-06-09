5b88eac37e6445d0c983e83ed214d7f3eccfc400
Md Shahriur Ahmed <tasnovakanta2000@gmail.com>
2022-06-09 21:19:42 +0600

    projects section updated

--- src/Components/MyProjects.js
+++ src/Components/MyProjects.js
@@ -6,93 +6,69 @@ import './MyProjects.css'
 
 const MyProjects = () => {
     return (
-        <div className='px-40'>
+        <div className='px-40 pb-40'>
             <h1 className='text-base-100 text-5xl uppercase font-semibold pb-8  mb-10'>MY <span className='text-secondary'>Projects</span></h1>
+            <div className='grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-10'>
+                <div className='mt-10 customShadow p-5'>
+                    <h1 className='text-center text-secondary text-3xl font-bold mb-5'>Dream Pictures</h1>
+                    <img className=' mb-5 ' src={first} alt="" />
+                    <p className='text-justify text-base-100 mt-5 mb-3'>
 
-            <div className='pb-40 grid grid-cols-1 lg:grid-cols-3  md:grid-cols-2 gap-5 justify-center items-center justify-items-center '>
-                <div class="card w-72 bg-base-100 customShadow ">
-                    <figure><img src={first} alt="Shoes" /></figure>
-                    <div class="card-body">
-                        <h2 class="text-2xl font-bold text-center text-secondary">Dream Pictures</h2>
-                        <p className='text-justify mb-3'>
-
-                            This is a wedding photography service providing website. Currently here
-                            some services are added and authentication system implemented. User can login/signup using google authentication also email, password
-                            based authentication system is implemented
+                        This is a wedding photography service providing website. Currently here
+                        some services are added and authentication system implemented. User can login/signup using google authentication also email, password
+                        based authentication system is implemented
 
 
-                        </p>
-                        <div class="card-actions justify-center">
-                            <div className='grid grid-cols-1 gap-3  justify-center justify-items-center'>
-                                <a href="https://dream-pictures.web.app/" target='_blank' rel='noreferrer' class="btn text-secondary  btn-primary w-36">View Project</a>
-                                <div className='grid grid-cols-1 gap-3 justify-between'>
-                                    <a href="https://github.com/shahriurahmed/dream-pictures" class="btn  btn-primary text-secondary w-36">Client </a>
+                    </p>
+                    <div className='grid grid-cols-1 gap-3  justify-center'><a className='px-8 bg-secondary customShadow  font-bold text-white  rounded-md py-2' href="https://dream-pictures.web.app/" target='_blank' rel='noreferrer'>Live Site</a>
+                        <a className='px-8 bg-secondary customShadow  font-bold text-white  rounded-md py-2' href="https://github.com/shahriurahmed/dream-pictures" target='_blank' rel='noreferrer'>Client Site</a>
 
-                                </div>
+                        <a className='px-8 bg-secondary customShadow  font-bold text-white  rounded-md py-2' href="/" target='_blank' rel='noreferrer'>N/A</a></div>
 
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-                <div class="card w-72 bg-base-100 customShadow ">
-                    <figure><img src={second} alt="Shoes" /></figure>
-                    <div class="card-body">
-                        <h2 class="text-2xl font-bold text-center text-secondary">Electro Point</h2>
-                        <p className='text-justify mb-3'>
-
-                            This is an Inventory Management website. This inventory management
-                            system is related to laptop items. Here different suppliers can manage their
-                            products. where users can Login/registration by google authentication/
-                            Email, Password.
-
-
-                        </p>
-                        <div class="card-actions justify-center">
-                            <div className='grid grid-cols-1 gap-3  justify-center justify-items-center'>
-                                <a href="https://electro-point.web.app/" target='_blank' rel='noreferrer' class="btn text-secondary  btn-primary w-36">View Project</a>
-                                <div className='grid grid-cols-2 gap-3 justify-between'>
-                                    <a href="https://github.com/shahriurahmed/electro-point-client" class="btn  btn-primary text-secondary w-28">Client </a>
-                                    <a href="https://github.com/shahriurahmed/electro-point-server" class="btn btn-primary w-28 text-secondary ">Server </a>
-                                </div>
-
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-                <div class="card w-72 bg-base-100 customShadow ">
-                    <figure><img src={third} alt="Shoes" /></figure>
-                    <div class="card-body">
-                        <h2 class="text-2xl font-bold text-center text-secondary">BD Tools</h2>
-                        <p className='text-justify mb-3'>
-
-                            This is a Manufacturer website where users can Login/registration by google
-                            authentication & User name, Email, Password, User & Admin Role separated
-                            Dashboard. Add products, manage all orders & Manage all Products & delete
-                            products, upgraded  by Admin
-
-
-                        </p>
-                        <div class="card-actions justify-center">
-                            <div className='grid grid-cols-1 gap-3  justify-center justify-items-center'>
-                                <a href="https://bd-tools-20f34.firebaseapp.com/" target='_blank' rel='noreferrer' class="btn text-secondary  btn-primary w-36">View Project</a>
-                                <div className='grid grid-cols-2 gap-3 justify-between'>
-                                    <a href="https://github.com/shahriurahmed/BD-Tools-client-side" class="btn  btn-primary text-secondary w-28">Client </a>
-                                    <a href="https://github.com/shahriurahmed/BD-Tools_server-side" class="btn btn-primary w-28 text-secondary ">Server </a>
-                                </div>
-
-                            </div>
-
-                        </div>
-                    </div>
                 </div>
 
 
+                <div className='mt-10 customShadow p-5'>
+                    <h1 className='text-center text-3xl font-bold mb-5 text-secondary'>Electro Point</h1>
+                    <img className='mb-5 ' src={second} alt="" />
+                    <p className='text-justify text-base-100 mt-5 mb-3'>
 
+                        This is an Inventory Management website. This inventory management
+                        system is related to laptop items. Here different suppliers can manage their
+                        products. where users can Login/registration by google authentication/
+                        Email, Password also can add/delete item.
 
+                    </p>
+                    <div className='grid grid-cols-1 gap-3  justify-center'>
+
+                        <a className='px-8 bg-secondary customShadow  font-bold text-white  rounded-md py-2' href="https://electro-point.web.app/" target='_blank' rel='noreferrer'>Live Site</a>
+
+                        <a className='px-8 bg-secondary customShadow  font-bold text-white  rounded-md py-2' href="https://github.com/shahriurahmed/electro-point-client" target='_blank' rel='noreferrer'>Client Site</a>
+
+                        <a className='px-8 bg-secondary customShadow  font-bold text-white  rounded-md py-2' href="https://github.com/shahriurahmed/electro-point-server" target='_blank' rel='noreferrer'>Server Site</a></div>
+
+                </div>
+
+                <div className='mt-10 customShadow p-5'>
+                    <h1 className='text-center text-3xl font-bold mb-5 text-secondary'>BD Tools</h1>
+                    <img className='mb-5 ' src={third} alt="" />
+                    <p className='text-justify text-base-100 mt-5 mb-3'>
+
+                        This is a Manufacturer website where users can Login/registration by google authentication & User name, Email, Password, User & Admin Role separated Dashboard. Add products, manage all orders & Manage all Products & delete products, upgraded by Admin.
+
+                    </p>
+                    <div className='grid grid-cols-1 gap-3  justify-center'>
+
+                        <a className='px-8 bg-secondary customShadow  font-bold text-white  rounded-md py-2' href="https://bd-tools-20f34.firebaseapp.com/" target='_blank' rel='noreferrer'>Live Site</a>
+
+                        <a className='px-8 bg-secondary customShadow  font-bold text-white  rounded-md py-2' href="https://github.com/shahriurahmed/BD-Tools-client-side" target='_blank' rel='noreferrer'>Client Site</a>
+
+                        <a className='px-8 bg-secondary customShadow  font-bold text-white  rounded-md py-2' href="https://github.com/shahriurahmed/BD-Tools_server-side" target='_blank' rel='noreferrer'>Server Site</a></div>
+                </div>
 
             </div>
+
+
         </div>
     );
 };
